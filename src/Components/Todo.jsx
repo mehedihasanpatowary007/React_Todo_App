c085c860e3b2de48527c2422d1ba0d848438fc4a
Mehedi Hasan <hasanmdrifat77@gmail.com>
2024-08-01 09:41:50 +0600

    add

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -14,7 +14,7 @@ const Todo = (props) => {
     <article className="flex justify-between items-center border-2 border-black p-3 m-5 rounded bg-gradient-to-r from-blue-400 to-violet-600">
       <div>
         <h2 className="text-2xl font-semibold uppercase italic">{title}</h2>
-        <p className="text-[18px] italic">{desc}</p>
+        <p className="text-[18px] italic break-words">{desc}</p>
       </div>
       <div>
         <button className="text-red-500 text-3xl opacity-100 hover:opacity-80 duration-500" onClick={ () => {
